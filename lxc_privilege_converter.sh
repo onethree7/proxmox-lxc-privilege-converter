cc11e804940ab5a82181541e44699179dfb01506
onethree7 <onethree.seven@yahoo.de>
2023-11-15 00:58:33 +0100

    Updated lxc_privilege_converter.sh with summary report and improved error handling

--- lxc_privilege_converter.sh
+++ lxc_privilege_converter.sh
@@ -21,11 +21,12 @@ ______________   _______________         .____     ____  ____________
      _/ ___\/  _ \ /    \  \/ // __ \_  __ \   __\/ __ \_  __ \        
      \  \__(  <_> )   |  \   /\  ___/|  | \/|  | \  ___/|  | \/        
       \___  >____/|___|  /\_/  \___  >__|   |__|  \___  >__|           
-          \/           \/          \/                 \/       v1.0   
-           From unprivileged to privileged and vice versa
-          Using vzdump backup / restore to rewrite metadata
-         Will use snapshot method. Ensure taking snaps works.
-    Assuming you have enough free space in images and target storage.
+          \/           \/          \/                 \/       v1.01   
+         Welcome to the Proxmox LXC Privilege Converter Script!
+    This script simplifies the process of converting LXC containers for
+privileged and unprivileged modes using the vzdump backup and restore method. 
+   Ensure that your PVE has enough free space in backup and target storage.
+    GitHub: https://github.com/onethree7/proxmox-lxc-privilege-converter
 EOF
 }
 
@@ -69,24 +70,25 @@ select_backup_storage() {
     done
 }
 
-# perform backup
+# perform backup, grab path and success status
 backup_container() {
     echo -e "Performing backup of container $CONTAINER_ID...\n"
     vzdump_output=$(mktemp)
     vzdump "$CONTAINER_ID" --compress zstd --storage "$BACKUP_STORAGE" --mode snapshot | tee "$vzdump_output"
     BACKUP_PATH=$(awk '/tar.zst/ {print $NF}' "$vzdump_output" | tr -d "'")
-    rm "$vzdump_output"
 
-    if [ -z "$BACKUP_PATH" ]; then
-        echo "Backup of container $CONTAINER_ID failed"
+    if [ -z "$BACKUP_PATH" ] || ! grep -q "Backup job finished successfully" "$vzdump_output"; then
+        rm "$vzdump_output"
+        echo "Backup of container $CONTAINER_ID failed or was incomplete."
         exit 1
     fi
-    echo -e "Backup of container $CONTAINER_ID successful\n"
+
+    rm "$vzdump_output"
 }
 
 # select target storage for the new container
 select_target_storage() {
-    echo -e "Select target storage for the new container:\n"
+    echo -e "\nSelect target storage for the new container:\n"
     target_storages=$(pvesm status --content images | awk '{if(NR>1)print $1}')
     select opt in $target_storages; do
         if [ -n "$opt" ]; then
@@ -144,11 +146,11 @@ perform_conversion() {
 
 # manage LXC states
 manage_lxc_states() {
-    read -r -p "Shutdown source and start target container? [Y/n]: " user_choice
-    user_choice=${user_choice:-Y}
+    read -r -p "Shutdown source and start target container? [Y/n]: " statechange_choice
+    statechange_choice=${statechange_choice:-Y}
 
-    if [[ $user_choice =~ ^[Yy]([Ee][Ss])?$ ]]; then
-        echo "Please note: If the container does not shut down within 3 minutes, a forced termination will be initiated."
+    if [[ $statechange_choice =~ ^[Yy]([Ee][Ss])?$ ]]; then
+        echo "Please note: If the container does not shut down within 3 minutes, you can choose to force the shutdown."
         pct shutdown "$CONTAINER_ID"
         local counter=0
         local timeout=180
@@ -163,11 +165,7 @@ manage_lxc_states() {
         done
 
         if [ $counter -ge $timeout ]; then
-            echo -e "Container $CONTAINER_ID did not shut down as expected, initiating forced termination.\n"
-            if ! pkill -9 -f "lxc-start -F -n $CONTAINER_ID"; then
-                echo -e "Failed to force shutdown container $CONTAINER_ID\n"
-                exit 1
-            fi
+            prompt_for_forced_shutdown
         fi
 
         if ! pct start "$NEW_CONTAINER_ID"; then
@@ -180,13 +178,29 @@ manage_lxc_states() {
     fi
 }
 
+# Prompt for forced shutdown, modular for manage_lxc_states
+prompt_for_forced_shutdown() {
+    read -r -p "Timeout reached. Forcefully shutdown the container? [Y/n]: " force_shutdown_choice
+    force_shutdown_choice=${force_shutdown_choice:-Y}
+
+    if [[ $force_shutdown_choice =~ ^[Yy]([Ee][Ss])?$ ]]; then
+        echo "Initiating forced termination of container $CONTAINER_ID."
+        if ! pkill -9 -f "lxc-start -F -n $CONTAINER_ID"; then
+            echo -e "Failed to force shutdown container $CONTAINER_ID\n"
+            exit 1
+        fi
+        echo "Forced termination of container $CONTAINER_ID completed."
+    else
+        echo "Skipping forced termination. Please manually manage the container state."
+    fi
+}
+
 # ask for cleanup of temporary backup files
 cleanup_temp_files() {
-    read -r -p "Do you want to clean up temporary backup files? "$BACKUP_PATH" [Y/n]: " cleanup_choice
+    read -r -p "Do you want to clean up temporary backup files? $BACKUP_PATH [Y/n]: " cleanup_choice
     cleanup_choice=${cleanup_choice:-Y}
 
     if [[ $cleanup_choice =~ ^[Yy]([Ee][Ss])?$ ]]; then
-        echo "Cleaning up temporary backup files..."
         if rm -f "$BACKUP_PATH"; then
             echo "Temporary backup files cleaned up successfully."
         else
@@ -198,7 +212,27 @@ cleanup_temp_files() {
     echo
 }
 
-main_menu() {
+# Summary function
+summarize_actions() {
+    echo -e "\n=================================================="
+    echo "Conversion Summary Report"
+    echo "=================================================="
+    echo "Source Container: $CONTAINER_ID ($CONTAINER_NAME)"
+    echo "Backup Storage: $BACKUP_STORAGE"
+    echo "Backup Path: $BACKUP_PATH"
+    echo "Target Storage for New Container: $TARGET_STORAGE"
+    echo "New Container: $NEW_CONTAINER_ID ($CONTAINER_NAME)"
+    echo "Privilege Conversion: $(if [ "$UNPRIVILEGED_FLAG" = true ]; then echo "Unprivileged to Privileged"; else echo "Privileged to Unprivileged"; fi)"
+    if [ "$statechange_choice" = "Y" ] || [ "$statechange_choice" = "y" ]; then
+        echo "Container State Changes: Source Stopped, Target Started"
+    else
+        echo "Container State Changes: No Changes Made"
+    fi
+    echo "Cleanup of Temporary Files: $(if [ "$cleanup_choice" = "Y" ] || [ "$cleanup_choice" = "y" ]; then echo "Performed"; else echo "Skipped"; fi)"
+    echo -e "==================================================\n"
+}
+
+main() {
     banner
     check_root
     select_container
@@ -208,7 +242,8 @@ main_menu() {
     perform_conversion
     manage_lxc_states
     cleanup_temp_files
+    summarize_actions
 }
 
 # Main script execution
-main_menu
+main
